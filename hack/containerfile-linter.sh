6a6d2d141a73561843222b0b1760825778733371
Bharath B <bhb@redhat.com>
2025-05-22 16:53:37 +0530

    ESO-47: Bootstrap main branch with configs to build catalog images

---
+++ hack/containerfile-linter.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+declare -a CONTAINERFILES
+declare -a EXTERNAL_SECRETS_OPERATOR_CONTAINERFILES
+
+
+linter()
+{
+	containerfiles=("$@")
+	for containerfile in "${containerfiles[@]}"; do
+		if [[ ! -f "${containerfile}" ]]; then
+			echo "[$(date)] -- ERROR -- ${containerfile} does not exist"
+			exit 1
+		fi
+		echo "[$(date)] -- INFO  -- running linter on ${containerfile}"
+		if ! podman run --rm -i -e "HADOLINT_FAILURE_THRESHOLD=error" ghcr.io/hadolint/hadolint < "${containerfile}" ; then
+			exit 1
+		fi
+	done
+}
+
+containerfile_linter()
+{
+	if [[ "${#CONTAINERFILES[@]}" -gt 0 ]]; then
+		linter "${CONTAINERFILES[@]}"
+		return
+	fi
+	mapfile -t EXTERNAL_SECRETS_OPERATOR_CONTAINERFILES < <(find . -type f -name 'Containerfile*' '!' -path './external-secrets/*' '!' -path './external-secrets-operator/*')
+	echo "[$(date)] -- INFO  -- running linter on ${EXTERNAL_SECRETS_OPERATOR_CONTAINERFILES[*]}"
+	linter "${EXTERNAL_SECRETS_OPERATOR_CONTAINERFILES[@]}"
+}
+
+##############################################
+###############  MAIN  #######################
+##############################################
+
+if [[ $# -ge 1 ]]; then
+	CONTAINERFILES=("$@")
+	echo "[$(date)] -- INFO  -- running linter on $*"
+fi
+
+containerfile_linter
+
+exit 0
